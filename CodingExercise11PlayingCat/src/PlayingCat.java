242c5f3d33c7654efbd7c0b26fe6a8011f0fbee2
Moyosore Omoniyi <moyosoreo@hardcorebiometric.com>
2022-02-11 23:55:31 +0100

    Added .gitignore file

--- CodingExercise11PlayingCat/src/PlayingCat.java
+++ CodingExercise11PlayingCat/src/PlayingCat.java
@@ -13,15 +13,15 @@ public class PlayingCat {
 
                 // OR
 
-public class PlayingCat {
-    private static final boolean BOOLEAN_VALUE = true;
-    public static boolean isCatPlaying(boolean summer, int temperature){
-        //    summer = true;
-        if((summer == true) && (temperature >=25 && temperature <=45)){
-            return BOOLEAN_VALUE;
-        }else if((summer == false) && (temperature >= 25 && temperature <= 35)){
-            return BOOLEAN_VALUE;
-        }else
-            return false;
-    }
-}
+//public class PlayingCat {
+//    private static final boolean BOOLEAN_VALUE = true;
+//    public static boolean isCatPlaying(boolean summer, int temperature){
+//        //    summer = true;
+//        if((summer == true) && (temperature >=25 && temperature <=45)){
+//            return BOOLEAN_VALUE;
+//        }else if((summer == false) && (temperature >= 25 && temperature <= 35)){
+//            return BOOLEAN_VALUE;
+//        }else
+//            return false;
+//    }
+//}
--- CodingExercise13NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
+++ CodingExercise13NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
@@ -1,9 +1,4 @@
 public class NumberOfDaysInMonth {
 
-    public static boolean isLeapYear(int year) {
-        switch(year) {
-            case year < 1:
 
-        }
-    }
 }
