2ce663f002aa2940ca0718fda2a78d1b0a57db8d
Moyosore Omoniyi <71763864+MoyosoreOmoniyi@users.noreply.github.com>
2022-02-12 09:16:16 +0100

    Update main.java

--- CodingExercise1SpeedConverter/src/main.java
+++ CodingExercise1SpeedConverter/src/main.java
@@ -1,9 +1,8 @@
 public class main {
 
     public static void main(String[] args) { // I typed p s v m
-        //SpeedConverter.toMilesPerHour(10.5); // I just called this class. Since in the original class, I didn't ask it to print, hence, we print here.
 
-        long milesPerHour = SpeedConverter.toMilesPerHour(10.5); // I assigned the method to a variable, now I can print out.
+        long milesPerHour = SpeedConverter.toMilesPerHour(10.5);
         System.out.println("Miles Per Hour is " + milesPerHour);
 
         SpeedConverter.printConversion(10.5);
